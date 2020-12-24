2a960ed4781d34c4525fa92afd264d9bc5e7a18a
ben avrahami <dontcallme@illcall.you>
2020-12-24 22:52:44 +0200

    added version to main

--- envolved/__init__.py
+++ envolved/__init__.py
@@ -1,5 +1,6 @@
+from envolved._version import __version__
 from envolved.schema import Schema
 from envolved.envvar import EnvVar
 from envolved.exceptions import MissingEnvError
 
-__all__ = ['Schema', 'EnvVar', 'MissingEnvError']
+__all__ = ['__version__', 'Schema', 'EnvVar', 'MissingEnvError']
